14ac1d268a515f2852f0aa34019ff57909d8b37b
Alisher <turdimuratovash@gmail.com>
2023-07-31 12:53:27 +0500

    CSS8dars
    24-gr o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,48 @@
+byWrap = document.getElementsByTagName("div").namedItem("wrap")
+byFormGroup = document.getElementsByTagName("div").namedItem("form-group")
+byLabel1 = document.getElementsByTagName("label").namedItem("label1")
+byLabel2= document.getElementsByTagName("label").namedItem("label2")
+byLabel3 = document.getElementsByTagName("label").namedItem("label3")
+byLabel4 = document.getElementsByTagName("label").namedItem("label4")
+byLabel5 = document.getElementsByTagName("label").namedItem("label5")
+byLabel6 = document.getElementsByTagName("label").namedItem("label6")
+byLabel7 = document.getElementsByTagName("label").namedItem("label7")
+byLabel8 = document.getElementsByTagName("label").namedItem("label8")
+byLabel9 = document.getElementsByTagName("label").namedItem("label9")
+byLabel10 = document.getElementsByTagName("label").namedItem("label10")
+byLabel11 = document.getElementsByTagName("label").namedItem("label11")
+byLabel12 = document.getElementsByTagName("label").namedItem("label12")
+byLabel13 = document.getElementsByTagName("label").namedItem("label13")
+byLabel14 = document.getElementsByTagName("label").namedItem("label14")
+byLabel15 = document.getElementsByTagName("label").namedItem("label15")
+byLabel16 = document.getElementsByTagName("label").namedItem("label16")
+byLabel17 = document.getElementsByTagName("label").namedItem("label17")
+byLabel18 = document.getElementsByTagName("label").namedItem("label18")
+
+byInput1 = document.getElementsByTagName("input").namedItem("input1")
+byFirstName = document.getElementsByTagName("input").namedItem("firstName")
+byLastName = document.getElementsByTagName("input").namedItem("last Name")
+byEmail = document.getElementsByTagName("input").namedItem("email")
+byPrEmail = document.getElementsByTagName("input").namedItem("premail")
+byPassword = document.getElementsByTagName("input").namedItem("password")
+
+
+byWrap.style.width = "1200px"
+byWrap.style.margin = "0 auto"
+
+byFormGroup.style.width = "500px"
+byFormGroup.style.margin = "0 auto"
+byFormGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)"
+byFormGroup.style.padding = "1rem 1.2rem"
+byFormGroup.style.boxSizing = "border-box"
+
+
+
+
+
+
+
+
+
+
+
